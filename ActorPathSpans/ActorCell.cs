f243e8f09fc580f05b2fa7884b6dffd64b038940
Aaron Stannard <aaron@petabridge.com>
2020-02-12 13:40:18 -0600

    initial build of ActorPath classes

---
+++ ActorPathSpans/ActorCell.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ActorCell.cs" company="Petabridge, LLC">
+//      Copyright (C) 2015 - 2020 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace Akka.Actor
+{
+    public static class ActorCell
+    {
+        public const int UndefinedUid = 0;
+
+        /// <summary>
+        /// TBD
+        /// </summary>
+        /// <param name="name">TBD</param>
+        /// <returns>TBD</returns>
+        public static NameAndUid SplitNameAndUid(string name)
+        {
+            var i = name.IndexOf('#');
+            return i < 0
+                ? new NameAndUid(name, UndefinedUid)
+                : new NameAndUid(name.Substring(0, i), Int32.Parse(name.Substring(i + 1)));
+        }
+    }
+}
---
+++ ActorPathSpans/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ActorPathSpans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
